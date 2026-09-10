d2e2c55d9c221aca799926aa07524f4b128b017d
Gabriel Andrade <gabrielandrade@k13.com.br>
2026-09-09 23:14:08 -0300

    ATV 2: cria rotas com parametro

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Route::get('/', function () { return view('welcome'); });
 Route::get('/sobre', function () { return 'Sobre o projeto de alunos'; });
 Route::get('/alunos', function () { return 'Lista de alunos'; });
 Route::get('/contato', function () { return 'Contato da escola'; });
+Route::get('/produto/{id}', function (string $id) { return 'Produto: '.$id; });
+Route::get('/categoria/{id}', function (string $id) { return 'Categoria: '.$id; });
+Route::get('/usuario/{id}', function (string $id) { return 'Usuario: '.$id; });
