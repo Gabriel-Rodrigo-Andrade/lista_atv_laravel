06b52039c9bea2cc37a84d86d8e11c349b0cfcfd
Gabriel Andrade <gabrielandrade@k13.com.br>
2026-09-09 23:14:08 -0300

    ATV 6: cria views principais e conecta ao controller

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class AlunoController extends Controller
 {
-    public function index(): string { return 'Lista de alunos'; }
-    public function create(): string { return 'Cadastrar aluno'; }
+    public function index(): View { return view('alunos.index'); }
+    public function create(): View { return view('alunos.create'); }
     public function store(Request $request): string { return 'Salvar aluno'; }
-    public function show(string $aluno): string { return 'Aluno: '.$aluno; }
-    public function edit(string $aluno): string { return 'Editar aluno: '.$aluno; }
+    public function show(string $aluno): View { return view('alunos.show'); }
+    public function edit(string $aluno): View { return view('alunos.edit'); }
     public function update(Request $request, string $aluno): string { return 'Atualizar aluno: '.$aluno; }
     public function destroy(string $aluno): string { return 'Excluir aluno: '.$aluno; }
 }
---
+++ resources/views/alunos/create.blade.php
@@ -0,0 +1,1 @@
+<h1>Cadastrar aluno</h1>
