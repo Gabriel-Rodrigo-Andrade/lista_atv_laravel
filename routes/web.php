d87791184d58fbd76fafbdd352fc15533e9f1668
Gabriel Andrade <gabrielandrade@k13.com.br>
2026-09-09 23:14:08 -0300

    ATV 1: cria rotas sobre, alunos e contato

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
+Route::get('/sobre', function () { return 'Sobre o projeto de alunos'; });
+Route::get('/alunos', function () { return 'Lista de alunos'; });
+Route::get('/contato', function () { return 'Contato da escola'; });
