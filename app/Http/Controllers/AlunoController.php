c53fe725fc540097a1d85ac21e95a9ed91521fb1
Gabriel Andrade <gabrielandrade@k13.com.br>
2026-09-09 23:14:08 -0300

    ATV 4: implementa as sete acoes e rotas do recurso alunos

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    public function index(): string { return 'Lista de alunos'; }
+    public function create(): string { return 'Cadastrar aluno'; }
+    public function store(Request $request): string { return 'Salvar aluno'; }
+    public function show(string $aluno): string { return 'Aluno: '.$aluno; }
+    public function edit(string $aluno): string { return 'Editar aluno: '.$aluno; }
+    public function update(Request $request, string $aluno): string { return 'Atualizar aluno: '.$aluno; }
+    public function destroy(string $aluno): string { return 'Excluir aluno: '.$aluno; }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () { return view('welcome'); });
 Route::get('/sobre', function () { return 'Sobre o projeto de alunos'; });
-Route::get('/alunos', function () { return 'Lista de alunos'; });
+Route::resource('alunos', \App\Http\Controllers\AlunoController::class);
 Route::get('/contato', function () { return 'Contato da escola'; });
 Route::get('/produto/{id}', function (string $id) { return 'Produto: '.$id; });
 Route::get('/categoria/{id}', function (string $id) { return 'Categoria: '.$id; });
